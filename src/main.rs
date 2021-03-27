fda54a5d33a1bf9d730c98cca4b3ef110a9932c8
Dominic Simone <dominic.simone@gmail.com>
2021-03-27 16:48:09 -0700

    Added test images

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use image::io::Reader as ImageReader;
 
 fn main() {
-    let img = open("./test/tree_256.png");
-    let img = img.resize(16, 16, image::imageops::FilterType::Gaussian);
-    println!("{:?}", img);
+    let img = open("./test/succulent_512.png");
+    gradients(small(img));
 }
 
 fn open(path: &str) -> image::DynamicImage {
     ImageReader::open(path).unwrap().decode().unwrap()
+}
+
+fn gradients(image: image::DynamicImage) {
+    let gray = image.grayscale();
+    println!("{:?}", gray);
+}
+
+fn small(image: image::DynamicImage) -> image::DynamicImage {
+    image.resize(16, 16, image::imageops::FilterType::Gaussian)
 }
