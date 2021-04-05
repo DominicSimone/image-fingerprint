15c48f76f44404355960ee44f72b27fabfbb6988
Dominic Simone <dominic.simone@gmail.com>
2021-04-04 22:43:35 -0700

    Runs, doesn't have correct output

--- src/main.rs
+++ src/main.rs
@@ -46,8 +46,14 @@ impl Ord for Corner {
 }
 
 fn main() {
-    let /*mut*/ img = open("./test/succulent_512.png");
-    shi_tomasi(&small(&img), 5);
+    let mut img = open("./test/succulent_512.png");
+    let corners = shi_tomasi(&small(&img), 7);
+    for c in corners {
+        let x = c.index as u32 % img.width();
+        let y = (c.index as u32 - x) / img.width();
+        mark(&mut img, x, y, 5);
+    }
+    save(&img);
 }
 
 fn open(path: &str) -> DynamicImage {
@@ -63,7 +69,6 @@ fn gradients(image: &DynamicImage) -> (DynamicImage, DynamicImage) {
     (grad_x, grad_y)
 }
 
-// This can be optimized to only iterate over grad_x and grad_y once each, rather than twice
 fn second_moment_prep(
     gradient_x: &DynamicImage,
     gradient_y: &DynamicImage,
@@ -82,11 +87,7 @@ fn second_moment_prep(
         .collect()
 }
 
-/**
- *
- * @param window_size the size of the window used to find features, must be odd
- */
-pub fn shi_tomasi(image: &DynamicImage, window_size: usize) {
+fn shi_tomasi(image: &DynamicImage, window_size: usize) -> Vec<Corner>{
     assert!(window_size % 2 != 0);
 
     let (grad_x, grad_y) = gradients(image);
@@ -95,6 +96,7 @@ pub fn shi_tomasi(image: &DynamicImage, window_size: usize) {
     // Window over sm_data and find eigen values of constructed second moment matrix
     let width = image.width() as usize;
     let height = image.height() as usize;
+
     let mut eigenvalues: Vec<(f32, f32)> = vec![(0., 0.); width * height];
     let mut corner_heap: BinaryHeap<Corner> = BinaryHeap::new();
 
@@ -103,7 +105,8 @@ pub fn shi_tomasi(image: &DynamicImage, window_size: usize) {
     let mut sumy2: u32 = 0;
     for row in 0..(height - window_size) {
         for w_start in 0..(width - window_size) {
-
+            let w_center =
+                (row * width) + w_start + (row * window_size / 2) + (window_size / 2) + 1;
             for w_row in 0..window_size {
                 for p in 0..window_size {
                     let index = (row * width) + w_start + (w_row * width) + p;
@@ -112,8 +115,6 @@ pub fn shi_tomasi(image: &DynamicImage, window_size: usize) {
                     sumy2 += sm_data[index].2;
                 }
             }
-            let w_center =
-                (row * width) + w_start + (row * window_size / 2) + (window_size / 2) + 1;
             // TODO error handling for when the eigenvalue calculation fails
             let eigens =
                 linalg::eigenvalues2x2(sumx2 as f32, sumxy as f32, sumxy as f32, sumy2 as f32)
@@ -125,8 +126,37 @@ pub fn shi_tomasi(image: &DynamicImage, window_size: usize) {
             eigenvalues[w_center] = eigens;
         }
     }
-    // TODO iterate over eigenvalues and select corners that are local maximums
-    // TODO mark the image with corners found (flagged feature?)
+
+    // iterate over eigenvalues and select corners that are local maximums
+    for row in 0..(height - window_size) {
+        for w_start in 0..(width - window_size) {
+            let w_center =
+                (row * width) + w_start + (row * window_size / 2) + (window_size / 2) + 1;
+            let mut local_max = true;
+            let center_max = eigenvalues[w_center].0.max(eigenvalues[w_center].1);
+            'window: for w_row in 0..window_size {
+                for p in 0..window_size {
+                    let i = (row * width) + w_start + (w_row * width) + p;
+                    if i != w_center && eigenvalues[i].0.max(eigenvalues[i].1) > center_max {
+                        local_max = false;
+                        break 'window;
+                    }
+                }
+            }
+            if local_max {
+                corner_heap.push(Corner {
+                    index: w_center,
+                    eigens: eigenvalues[w_center],
+                })
+            }
+        }
+    }
+    let mut corners: Vec<Corner> = Vec::new();
+    let size = std::cmp::min(corner_heap.len(), 10);
+    for _ in 0..size {
+        corners.push(corner_heap.pop().unwrap());
+    }
+    corners
 }
 
 pub fn small(image: &DynamicImage) -> DynamicImage {
@@ -139,10 +169,13 @@ pub fn save(image: &DynamicImage) {
 
 pub fn mark(image: &mut DynamicImage, x: u32, y: u32, r: u32) {
     let img = image.as_mut_rgb8().unwrap();
-
-    for j in x - r..=x + r {
-        for k in y - r..=y + r {
-            if (j == x - r || j == x + r || k == y - r || k == y + r) {
+    let xmin = if x >= r { x - r } else { 0 };
+    let xmax = if x <= img.width() - r {x + r} else {img.width()};
+    let ymin = if y >= r { y - r } else { 0 };
+    let ymax = if y <= img.height() - r {y + r} else {img.height()};
+    for j in xmin..=xmax {
+        for k in ymin..=ymax {
+            if (j == xmin || j == xmax || k == ymin || k == ymax) {
                 img.put_pixel(j, k, Rgb([255, 0, 0]));
             }
         }
